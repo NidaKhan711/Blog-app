6a2e1b5614c48f0d066fb2a8929f997999b0099b
nidabatool <nida711711@gamil.com>
2024-10-29 18:39:53 +0500

    create pages and using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Signin from './Pages/Signin'
+import Signup from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Projact from './Pages/Projact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    
+    <Routes>
+      <Route path='/' element ={<Home/>}/>
+      <Route path='/about' element ={<About/>}/>
+      <Route path='/sign-in' element ={<Signin/>}/>
+      <Route path='/sign-up' element ={<Signup/>}/>
+      <Route path='/projact' element ={<Projact/>}/>
+      <Route path='/dashboard' element ={<Dashboard/>}/>
+    </Routes>
+
+    </>
   )
 }
 
---
+++ src/Pages/Projact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projact = () => {
+  return (
+    <div>Projact</div>
+  )
+}
+
+export default Projact
