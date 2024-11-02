d513344b61249bf15043a71aab86045194dafba7
nidabatool <nida711711@gamil.com>
2024-11-02 08:39:44 +0500

    create navbar and sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import {BrowserRouter, Routes,Route } from 'react-router-dom'
 import Signin from './Pages/Signin'
 import Signup from './Pages/SignUp'
 import Dashboard from './Pages/Dashboard'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Projact from './Pages/Projact'
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <>
     
+    <BrowserRouter>
+    <Header/>
     <Routes>
+  
       <Route path='/' element ={<Home/>}/>
       <Route path='/about' element ={<About/>}/>
       <Route path='/sign-in' element ={<Signin/>}/>
@@ -19,6 +23,7 @@ const App = () => {
       <Route path='/projact' element ={<Projact/>}/>
       <Route path='/dashboard' element ={<Dashboard/>}/>
     </Routes>
+    </BrowserRouter>
 
     </>
   )
